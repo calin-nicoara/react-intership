faf8f00d96ff2b70a2b9bdc598e9276aff598bc8
calin.nicoara <calin.nicoara@esolutions.ro>
2021-07-23 14:55:16 +0300

    Linked product form to React.

--- my-first-routing-project/src/pages/NewProductForm.js
+++ my-first-routing-project/src/pages/NewProductForm.js
@@ -1,18 +1,60 @@
+import {useState} from "react";
+
 export default function NewProductForm() {
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredDescription, setEnteredDescription] = useState('');
+  const [enteredPrice, setEnteredPrice] = useState('');
+
+  const enteredProductNameIsInvalid = enteredName.trim().length === 0;
+
+  function onNameChangedHandler(event) {
+    setEnteredName(event.target.value);
+  }
+
+  function onDescriptionChangedHandler(event) {
+    setEnteredDescription(event.target.value);
+  }
+
+  function onPriceChangedHandler(event) {
+    setEnteredPrice(event.target.value);
+  }
+
+  function onSubmitHandler(event) {
+    event.preventDefault();
+
+    const newProduct = {
+      name: enteredName,
+      description: enteredDescription,
+      price: enteredPrice
+    }
+
+    console.log(newProduct);
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmitHandler}>
         <div className="mb-3">
           <label htmlFor="productName" className="form-label">Product name</label>
-          <input type="text" className="form-control" id="productName" aria-describedby="emailHelp" />
+          <input type="text" className="form-control"
+                 value={enteredName}
+                 onChange={onNameChangedHandler}
+                 id="productName" />
+          {enteredProductNameIsInvalid && <p>Please enter a valid product</p>}
         </div>
         <div className="mb-3">
           <label htmlFor="productDescription" className="form-label">Product description</label>
-          <input type="text" className="form-control" id="productDescription" />
+          <input type="text"
+                 value={enteredDescription}
+                 onChange={onDescriptionChangedHandler}
+                 className="form-control" id="productDescription" />
         </div>
         <div className="mb-3">
           <label htmlFor="productPrice" className="form-label">Price</label>
-          <input type="number" className="form-control" id="productPrice" />
+          <input type="number"
+                 value={enteredPrice}
+                 onChange={onPriceChangedHandler}
+                 className="form-control" id="productPrice" />
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
