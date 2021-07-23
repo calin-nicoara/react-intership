35189523f3f7ec85538ebdc4cf9c1c74b5e6683b
calin.nicoara <calin.nicoara@esolutions.ro>
2021-07-23 16:26:50 +0300

    Added custom hooks

--- my-first-routing-project/src/hooks/UseFirstExample.js
+++ my-first-routing-project/src/hooks/UseFirstExample.js
@@ -1,13 +1,20 @@
 import {useEffect, useState} from "react";
 
-export default function useFirstExample() {
+export default function useFirstExample(validationFunction) {
   const [testState, setTestState] = useState("test");
 
   useEffect(() => {
     console.log("This is run one time!")
   }, [])
 
+  function setTestStateOnlyIfValid(value) {
+    if(validationFunction(value)) {
+      setTestState(value);
+    }
+  }
+
   return {
-    testStateValue: testState
+    testStateValue: testState,
+    setTestValue: setTestStateOnlyIfValid
   }
 }
--- my-first-routing-project/src/pages/TestingCustomHooks.js
+++ my-first-routing-project/src/pages/TestingCustomHooks.js
@@ -1,3 +1,16 @@
+import useFirstExample from "../hooks/UseFirstExample";
+
 export default function TestingCustomHooks() {
-  return <div>Hello there!</div>
+  const firstExample = useFirstExample((value) => value.length > 20);
+
+  console.log("RUNNING CUSTOM HOOKS");
+
+  function setTestValue() {
+    firstExample.setTestValue("aaaaaa bbbbbb ccccc dddd eeeeeee");
+  }
+
+  return <div>
+    Hello there! {firstExample.testStateValue}
+    <button onClick={setTestValue}>Set test value</button>
+  </div>
 }
