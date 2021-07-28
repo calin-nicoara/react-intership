a70cf413ca3887629f50e885e8aee82abc7fe506
calin.nicoara <calin.nicoara@esolutions.ro>
2021-07-28 17:47:16 +0300

    Finished initial pagination

--- food-list/food-list-react/src/common/Pagination.tsx
+++ food-list/food-list-react/src/common/Pagination.tsx
@@ -1,17 +1,32 @@
+import { useHistory } from "react-router-dom";
+
 export default function Pagination(props: any) {
+    const locationStateHistory = useHistory();
+
     const listItems = [];
+
+    function onClickPage(event: any) {
+        const number = Number(event.target.innerHTML)-1;
+        locationStateHistory.push({
+            pathname: "/food-list",
+            search: "?page=" + number + "&size=3"
+        })
+    }
+
     for(let i = 0; i < props.totalPages; i++) {
-        listItems.push(<li className="page-item"><a className="page-link" href="#">1</a></li>)
+        const classNames = i === Number(props.currentPage) ? "page-item active": "page-item"
+
+        listItems.push(<li key={i} className={classNames}>
+            <a className="page-link" onClick={onClickPage} href="#">{i+1}
+            </a>
+        </li>)
     }
 
     return (
         <nav aria-label="Page navigation example">
             <ul className="pagination">
                 <li className="page-item"><a className="page-link" href="#">Previous</a></li>
-
-                <li className="page-item"><a className="page-link" href="#">1</a></li>
-                <li className="page-item"><a className="page-link" href="#">2</a></li>
-                <li className="page-item"><a className="page-link" href="#">3</a></li>
+                {listItems}
                 <li className="page-item"><a className="page-link" href="#">Next</a></li>
             </ul>
         </nav>
